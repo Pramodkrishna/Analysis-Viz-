e31082ecf250b1b0561cbeb1a0e7be6d5b6346c6
pillutlapramod@gmail.com <pillutlapramod@gmail.com>
2017-06-12 17:36:41 -0400

    The house_pred.r is the final file to predict the house prices, things to be done are random forest and regression of validated model

---
+++ house_predict.R
@@ -0,0 +1,36 @@
+# "Prediction of House Prices (Regression Trees, Random Forests, Gradient Boosting Machine)"
+library(dplyr)
+library(tidyr)
+library(rpart)
+library(randomForest)
+library(ggplot2)
+library(gbm)
+library(Metrics)
+
+# Data preparation 
+
+# Load data
+
+train_pred <- read.csv("train.csv",stringsAsFactors = F)
+row.names(train_pred) <- train_pred$Id
+train_pred <- train_pred[,-1]
+train_pred[is.na(train_pred)] <- 0
+for(i in colnames(train_pred[,sapply(train_pred,is.character)])){
+  train_pred[,i] <- as.factor(train_pred[,i])
+}
+
+
+model_kag <- rpart(SalePrice ~., data = train_pred, method = "anova")
+predict_kag_pred <- predict(model_kag, test.csv)
+head(predict_kag_pred)
+head(train.csv$SalePrice)
+
+# RMSE
+Root_mean <- rmse(actual =  train.csv$SalePrice,predicted = predict_kag_pred)
+View(Root_mean)
+plot1 <- predict_kag_pred~train_num$SalePrice
+plot2 <- train_num$SalePrice~predict_kag_pred
+
+
+
+
